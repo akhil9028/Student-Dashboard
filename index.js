97d69f28694aae4b8abf2a01251fbe24f6d58f14
akhil9028 <111130878+akhil9028@users.noreply.github.com>
2023-04-24 14:07:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// alert("ahdk");
+
+// var a =document.getElementById("displayDiv");
+
+document.querySelector(".TE1").addEventListener("click",function (){
+    // alert("lkdsf");
+    document.querySelector(".displayDiv").innerHTML="<h4><b>TE1 Students :</b></h4>";
+})
+
+
+document.querySelector(".TE2").addEventListener("click",function (){
+    // alert("lkdsf");
+    document.querySelector(".displayDiv").innerHTML="<h4><b>TE2 Students :</b></h4>";
+    
+})
+
+
+document.querySelector(".TE3").addEventListener("click",function (){
+    // alert("lkdsf");
+    document.querySelector(".displayDiv").innerHTML="<h4><b>TE3 Students :</b></h4>";
+
+})
